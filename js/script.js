6aa1799523a350ececf1d2b50eb2b5edb36c0ea0
pablotroncoso <pablotroncoso@gmail.com>
2022-06-08 18:44:07 -0400

    Proyecto recuperando

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ const btn = document.querySelector('#cristales')
 btn.addEventListener('click', () => {
 
     Swal.fire({
-        title: 'Usando Sweet Alert!',
-        text: 'Dale Click!',
+        title: 'Felicidades encontraste el botón de descuento!',
+        text: 'Solicita tu descuento de 15% en compras.',
         icon: 'success',
-        confirmButtonText: ':)'
+        confirmButtonText: 'Aceptar'
 })
 })
+
+
